4713847c6f51ac68f552d49f5fee4649199a1da0
Szymon Ogniewski <szymonogniewski00@gmail.com>
2023-03-22 20:35:05 +0100

    c

--- src/pages/ContactBook/ContactBook.js
+++ src/pages/ContactBook/ContactBook.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getAllContactsThunk } from 'redux/contacts/contactsThunks';
-import { selectContacts } from 'redux/selectors';
 import { AddContactForm } from './AddContactForm/AddContactForm';
 import { ContactList } from './ContactList/ContactList';
 
 const Phonebook = () => {
-  const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getAllContactsThunk());
