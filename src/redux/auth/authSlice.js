9941e150b917a82eab0990ebb756b124b3111940
Szymon Ogniewski <szymonogniewski00@gmail.com>
2023-03-23 00:45:23 +0100

    C

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -39,33 +39,6 @@ const authSlice = createSlice({
       })
       .addMatcher(isRejectedAction, handleRejection)
       .addDefaultCase((state, action) => state);
-
-    // [register.fulfilled](state, action) {
-    //   state.user = action.payload.user;
-    //   state.token = action.payload.token;
-    //   state.isLoggedIn = true;
-    // },
-    // [signIn.fulfilled](state, action) {
-    //   state.user = action.payload.user;
-    //   state.token = action.payload.token;
-    //   state.isLoggedIn = true;
-    // },
-    // [signOut.fulfilled](state) {
-    //   state.user = { name: null, email: null };
-    //   state.token = null;
-    //   state.isLoggedIn = false;
-    // },
-    // [refreshUser.pending](state) {
-    //   state.isRefreshing = true;
-    // },
-    // [refreshUser.fulfilled](state, action) {
-    //   state.isLoggedIn = true;
-    //   state.isRefreshing = false;
-    //   state.user = action.payload;
-    // },
-    // [refreshUser.rejected](state) {
-    //   state.isRefreshing = false;
-    // },
   },
 });
 
